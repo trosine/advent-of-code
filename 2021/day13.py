c974bf325196c90e2236debef892133c0fd3912a
Tim Rosine <tim.rosine@gmail.com>
2021-12-13 22:10:43 -0600

    Improve 2021/13
    The main improvement here is performance, by only "folding" lit pixels.
    This also ends up reducing the complexity.
    
    I've also added a bit of whitespace to make some parts a bit easier to
    read.

--- 2021/day13.py
+++ 2021/day13.py
@@ -3,53 +3,53 @@
 https://adventofcode.com/2021/day/13
 """
 from collections import defaultdict
-from itertools import product
 
 import aoc
 
 PUZZLE = aoc.Puzzle(day=13, year=2021)
 
 
-def fold(grid, bottom_right, kind, crease):
+def fold(grid, axis, crease):
     """Fold the code"""
-    if kind[-1] == 'x':
+    if axis == 'x':
         variant = 0
-        step = -1
     else:
         variant = 1
-        step = 1
-    static_range = range(bottom_right[1 - variant] + 1)
-    offset_range = range(1, crease + 1)
-    # print(f'x={static_range}, y={offset_range}')
-    for pos, offset in product(static_range, offset_range):
-        source = (pos, crease + offset)[::step]
-        dest = (pos, crease - offset)[::step]
-        # print(f'{source} -> {dest}')
-        grid[dest] = max(grid[source], grid[dest])
-        del grid[source]
-    bottom_right[variant] = crease - 1
+    to_merge = [
+        pos
+        for pos in grid.keys()
+        if pos[variant] > crease
+        ]
+    for pos in to_merge:
+        if pos[variant] > crease:
+            dest = list(pos)
+            dest[variant] = crease - (pos[variant] - crease)
+            dest = tuple(dest)
+            # print(f'fold along {axis}={crease}: {pos} => {dest}')
+            grid[dest] = 1  # the only keys in the grid are "on"
+            del grid[pos]
 
 
 def solve(part='a'):
     """Solve puzzle"""
     grid = defaultdict(int)
     dots, folds = PUZZLE.input.split('\n\n')
-    bottom_right = (0, 0)
     for dot in dots.splitlines():
         pos = tuple(map(int, dot.split(',')))
-        bottom_right = list(map(max, bottom_right, pos))
         grid[pos] = 1
-    print(bottom_right)
+
     for instruction in folds.splitlines():
-        kind, crease = instruction.split('=')
-        fold(grid, bottom_right, kind[-1], int(crease))
+        axis, crease = instruction.split('=')
+        fold(grid, axis[-1], int(crease))
         if part == 'a':
             return sum(grid.values())
-    for row in range(bottom_right[1] + 1):
-        for col in range(bottom_right[0] + 1):
-            char = '#' if grid[(col, row)] else ' '
-            print(char, end='')
-        print()
+
+    max_x, max_y = map(max, *grid.keys())
+    for row in range(max_y + 1):
+        line = ''
+        for col in range(max_x + 1):
+            line += '#' if grid[(col, row)] else ' '
+        print(line)
     print()
     return input('Type in the code you see above: ').upper()
 
