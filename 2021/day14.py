9d921a62fb7587f23adde883466e07bec407e484
Tim Rosine <tim.rosine@gmail.com>
2021-12-13 23:53:28 -0600

    Solve 2021/14 part b
    The key was to realize that we only really needed to keep track of the
    number of occurances of each pair. During each expansion, the pairs
    completely mutate, which is why we create a new counter in every
    expansion.
    
    The final step is counting the first character of every pair, plus the
    last character of the original template.

--- 2021/day14.py
+++ 2021/day14.py
@@ -13,19 +13,32 @@ def solve(part='a'):
     """Solve puzzle"""
     cycles = 10 if part == 'a' else 40
     template, instructions = PUZZLE.input.split('\n\n')
-    # template = 'NNCB'
-    rules = {}
-    for rule in instructions.splitlines():
-        rules[rule[0:2]] = rule[0] + rule[-1]
+    # convert "AB -> C" to key="AB", value=("AC", "CB")
+    # the values become the new pairs that we keep track of
+    rules = {
+        rule[0:2]: (rule[0] + rule[-1], rule[-1] + rule[1])
+        for rule in instructions.splitlines()
+        }
+    pairs = Counter(
+        template[index:index+2]
+        for index in range(len(template) - 1)
+        )
+
     for _ in range(cycles):
-        new_template = ''
-        for index in range(len(template) - 1):
-            new_template += rules[template[index:index+2]]
-        template = new_template + template[-1]
-    counts = Counter(template)
+        new_pairs = Counter()
+        for pair, count in pairs.items():
+            for new_pair in rules[pair]:
+                new_pairs[new_pair] += count
+        pairs = new_pairs
+
+    # seed the counter with the last char (not included in the loop)
+    counts = Counter(template[-1])
+    for pair, count in pairs.items():
+        # the second character is the first char of a later pair
+        counts[pair[0]] += count
     return max(counts.values()) - min(counts.values())
 
 
 if __name__ == "__main__":
     PUZZLE.report_a(solve('a'))
-    # PUZZLE.report_b(solve('b'))
+    PUZZLE.report_b(solve('b'))
