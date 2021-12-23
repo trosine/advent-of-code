4731678c8daf370e8584e1466a937935ab145101
Tim Rosine <tim.rosine@gmail.com>
2021-12-22 20:56:25 -0600

    Clean up 2021/21
    I realized that I didn't really need the cycling, since the board has
    exactly 10 spaces - (100 + 101 + 102) % 10 == (100 + 1 + 2) % 10. The
    Die() class wasn't really providing any benefit besides combining 2
    statements (sum(rolls), and increasing the total rolls performed).

--- 2021/day21.py
+++ 2021/day21.py
@@ -9,18 +9,6 @@ import aoc
 PUZZLE = aoc.Puzzle(day=21, year=2021)
 
 
-class Die:
-    # pylint: disable=too-few-public-methods
-    """The die used in the game"""
-
-    def __init__(self):
-        self._iter = itertools.cycle(range(1, 101))
-        self.rolls = 0
-
-    def roll(self):
-        """Roll the die 3 times"""
-        self.rolls += 3
-        return sum(next(self._iter) for _ in range(3))
 
 
 def solve(part='a'):
@@ -32,19 +20,21 @@ def solve(part='a'):
         for player in PUZZLE.input.splitlines()
         ]
     # positions = [4, 8]
-    die = Die()
     scores = [0, 0]
     player = 0
+    rolls = 0
+    die = itertools.count(1)
     while True:
-        roll = die.roll()
-        position = (positions[player] + roll) % 10
+        rolls += 3
+        roll = sum(next(die) for _ in range(3))
+        # position 0 is really position 10
+        position = (positions[player] + roll) % 10 or 10
         positions[player] = position
-        scores[player] += position or 10  # position 0 is really position 10
+        scores[player] += position
         if scores[player] >= 1000:
-            print(f'rolls={die.rolls}, {scores}')
-            return scores[1-player] * die.rolls
+            # print(f'rolls={rolls}, {scores}')
+            return scores[1-player] * rolls
         player = 1 - player
-    print(positions)
     return None
 
 
