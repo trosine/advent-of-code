0a6d678720e80ca8331e5640c978af1a56ac4cd3
Tim Rosine <tim.rosine@gmail.com>
2022-12-25 22:44:15 -0600

    Solve 2022/15

---
+++ 2022/day15.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+"""
+https://adventofcode.com/2022/day/15
+"""
+import re
+
+import aoc
+from lib.point import Point2D
+
+PUZZLE = aoc.Puzzle(day=15, year=2022)
+PARSE = re.compile(
+    r"Sensor at x=([-0-9]+), y=([-0-9]+): "
+    r"closest beacon is at x=([-0-9]+), y=([-0-9]+)"
+)
+
+
+def line_range(line, sensor, distance):
+    """Provide a start/end that this sensor can see on the line"""
+    adjusted = distance - abs(line - sensor.y)
+    if adjusted < 0:
+        return None
+    result = (sensor.x - adjusted, sensor.x + adjusted)
+    return result
+
+
+def solve(part='a'):
+    """Solve puzzle"""
+    row = 2000000
+    sensors = {}
+    beacons = set()
+    for line in PUZZLE.input.splitlines():
+        groups = tuple(map(int, PARSE.search(line).groups()))
+        sensor = Point2D(*groups[:2])
+        beacon = Point2D(*groups[2:])
+        sensors[sensor] = sensor.distance(beacon)
+        beacons.add(beacon)
+    line = {}
+    if part == "a":
+        for sensor, distance in sensors.items():
+            adjusted = distance - abs(row - sensor.y)
+            for col in range(sensor.x - adjusted, sensor.x + adjusted + 1):
+                line[col] = True
+        for beacon in beacons:
+            if beacon.y == row:
+                line.pop(beacon.x, None)
+        return len(line)
+    grid_max = 4000000
+    for line in range(0, grid_max):
+        # print(line, end="\r")
+        ranges = []
+        for sensor, distance in sensors.items():
+            range_ = line_range(line, sensor, distance)
+            if range_:
+                ranges.append(range_)
+        end = 0
+        for range_ in sorted(ranges):
+            if range_[0] > end+1:
+                print(f"Found gap at {Point2D(end+1, line)}")
+                return grid_max*(end+1) + line
+            if range_[1] > end:
+                end = range_[1]
+    return None
+
+
+if __name__ == "__main__":
+    PUZZLE.report_a(solve('a'))
+    PUZZLE.report_b(solve('b'))
---
+++ lib/point.py
@@ -0,0 +1,52 @@
+"""Coordinate system point management"""
+
+from collections import namedtuple
+from operator import add, sub
+
+
+class PointFunctions:
+    """Functions that can be mixed in to handle management of coordinates"""
+
+    def __abs__(self):
+        return type(self)(*map(abs, self))
+
+    def __add__(self, right):
+        return type(self)(*map(add, self, right))
+
+    def __sub__(self, right):
+        return type(self)(*map(sub, self, right))
+
+    def distance(self, other):
+        """Return the manhattan distance to the other point"""
+        return sum(abs(self-other))
+
+    # def __repr__(self):
+    #     return repr(tuple(self))
+
+    def unit(self):
+        """Return a Point() with each axis normalized to -1, 0 or 1"""
+        elements = []
+        for element in self:  # pylint: disable=not-an-iterable
+            sign = 0
+            if element > 0:
+                sign = 1
+            elif element < 0:
+                sign = -1
+            elements.append(sign)
+        return type(self)(*elements)
+
+
+class Point2D(namedtuple("Point2D", ["x", "y"]), PointFunctions):
+    """A 2D Coordinate Point"""
+    cardinals = [
+        (-1, 0),
+        (1, 0),
+        (0, -1),
+        (0, 1),
+    ]
+    diagonals = [
+        (-1, -1),
+        (-1, 1),
+        (1, -1),
+        (1, 1),
+    ]
