4b43a9b9b4f20c6afe79066a60bdee27e8b886ac
Tim Rosine <tim.rosine@gmail.com>
2023-12-17 09:45:33 -0600

    Improve template
    * Use double-quotes for strings
    * Always run both solutions (both parts initially return None separately)

--- common/template.py
+++ common/template.py
@@ -7,14 +7,14 @@ import aoc
 PUZZLE = aoc.Puzzle(day=DAY, year=YEAR)
 
 
-def solve(part='a'):
+def solve(part="a"):
     """Solve puzzle"""
-    if part == 'a':
-        pass
+    if part == "b":
+        return None
     # print(PUZZLE.input)
     return None
 
 
 if __name__ == "__main__":
-    PUZZLE.report_a(solve('a'))
-    # PUZZLE.report_b(solve('b'))
+    PUZZLE.report_a(solve("a"))
+    PUZZLE.report_b(solve("b"))
